cb8728827d37171df98dd67347a95ee5087ce3fa
Art <d.art.shishkin@gmail.com>
2020-08-06 15:49:05 +0300

    Initial commit

---
+++ src/test/java/com/artarkatesoft/artsfgthymeleafcourse/ArtSfgThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artarkatesoft.artsfgthymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtSfgThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
