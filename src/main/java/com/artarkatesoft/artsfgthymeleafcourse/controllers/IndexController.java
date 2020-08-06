99214afc9830d49771ff612f8118e25495b60929
Art <d.art.shishkin@gmail.com>
2020-08-06 21:05:29 +0300

    Spring MVC Configuration (17)

--- src/main/java/com/artarkatesoft/artsfgthymeleafcourse/controllers/IndexController.java
+++ src/main/java/com/artarkatesoft/artsfgthymeleafcourse/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.artarkatesoft.artsfgthymeleafcourse.controller;
+package com.artarkatesoft.artsfgthymeleafcourse.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
