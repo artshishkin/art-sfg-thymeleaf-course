d14f63bfea1c25681c0d1c305eef0fa559c10200
Art <d.art.shishkin@gmail.com>
2020-08-07 11:33:37 +0300

    Returning Objects to Thymeleaf and accessing their properties (35-36)

--- src/main/java/com/artarkatesoft/artsfgthymeleafcourse/controllers/ProductController.java
+++ src/main/java/com/artarkatesoft/artsfgthymeleafcourse/controllers/ProductController.java
@@ -1,12 +1,30 @@
 package com.artarkatesoft.artsfgthymeleafcourse.controllers;
 
+import com.artarkatesoft.artsfgthymeleafcourse.domain.Product;
+import com.artarkatesoft.artsfgthymeleafcourse.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("product")
 public class ProductController {
-    @GetMapping("product")
-    public String getProduct(){
+
+    private final ProductService productService;
+
+    @GetMapping
+    public String getProduct() {
+        return "redirect:/index";
+    }
+
+    @GetMapping("{id}")
+    public String getProductById(@PathVariable Integer id, Model model) {
+        Product product = productService.getProduct(id);
+        model.addAttribute("product", product);
         return "product";
     }
 }
