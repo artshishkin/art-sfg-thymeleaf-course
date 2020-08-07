b42a2faad2caf78b021809a4c0a59e0892b7a362
Art <d.art.shishkin@gmail.com>
2020-08-07 12:37:31 +0300

    Iteration in Thymeleaf (41)

--- src/main/java/com/artarkatesoft/artsfgthymeleafcourse/controllers/IndexController.java
+++ src/main/java/com/artarkatesoft/artsfgthymeleafcourse/controllers/IndexController.java
@@ -1,12 +1,20 @@
 package com.artarkatesoft.artsfgthymeleafcourse.controllers;
 
+import com.artarkatesoft.artsfgthymeleafcourse.services.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final ProductService productService;
+
     @GetMapping
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("products", productService.listProducts());
         return "index";
     }
 }
